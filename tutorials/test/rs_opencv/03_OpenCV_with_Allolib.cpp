df9b814683c522b9ab65f080c85de400c4169cc6
Leo Qian <a2573705387@gmail.com>
2022-07-20 14:58:15 -0400

    update 03 to make it working

--- tutorials/test/rs_opencv/03_OpenCV_with_Allolib.cpp
+++ tutorials/test/rs_opencv/03_OpenCV_with_Allolib.cpp
@@ -1,96 +1,63 @@
-//
-// Created by Leo Qian on 7/17/22.
-//
-#include <iostream>
-#include <sstream>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/videoio.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/video.hpp>
-#include "al/app/al_App.hpp"
-#include "al/math/al_Mat.hpp"
-#include "librealsense2/rs.hpp"
+// License: Apache 2.0. See LICENSE file in root directory.
+// Copyright(c) 2017 Intel Corporation. All Rights Reserved.
+
+#include "librealsense2/rs.hpp" // Include RealSense Cross Platform API
+#include <opencv2/opencv.hpp>   // Include OpenCV API
 #include "cv-helpers.hpp"
 
-using namespace al;
-using namespace cv;
-using namespace rs2;
+int main(int argc, char *argv[]) try {
+    // Declare depth colorizer for pretty visualization of depth data
+//    rs2::colorizer color_map;
+    // set window size
 
-// using namespace rs2;
-class WallApp : public al::App {
-public:
-
-// Create a Pipeline - this serves as a top-level API for streaming and processing frames
-    rs2::pipeline pipe;
+    // Declare realsense config
     rs2::config cfg;
-//    rs2::align align_to;
+    cfg.enable_stream(RS2_STREAM_COLOR);
 
-    cv::Mat image;
-    cv::Mat colorMat;
-
-    const size_t windowWidth = 400;
-    const size_t windowHeight = 270;
-
-    WallApp() {
+    // Declare realsense align
+    rs2::align align_to(RS2_STREAM_COLOR);
 
 
-    }
+    // Declare RealSense pipeline, encapsulating the actual device and sensors
+    rs2::pipeline pipe;
+    // Start streaming with default recommended configuration
+    pipe.start();
 
-    void onCreate() {
-//        nav().pos(0, 0, 0);
-//        nav().faceToward(Vec3d(0, 0, 1), Vec3d(0, -1, 0));
+    using namespace cv;
+    const auto window_name = "Display Image";
+    namedWindow(window_name, WINDOW_AUTOSIZE);
 
+    while (waitKey(1) < 0 && getWindowProperty(window_name, WND_PROP_AUTOSIZE) >= 0) {
+        rs2::frameset data = pipe.wait_for_frames(); // Wait for next set of frames from the camera
 
-        // Configure and start the pipeline
-        cfg.enable_stream(RS2_STREAM_COLOR);
+        data = align_to.process(data);
 
-        pipe.start(cfg);
-    }
-
-    void onAnimate(double dt) {
-        namedWindow("rs_opencv", WINDOW_AUTOSIZE);
-
-        auto frames = pipe.wait_for_frames();
-        rs2::align align_to(RS2_STREAM_COLOR);
-        // align the frames
-        frames = align_to.process(frames);
-
-        auto color = frames.get_color_frame();
-
-        // For cameras that don't have RGB sensor, we'll map the pointcloud to infrared instead of color
-        if (!color)
-            color = frames.get_infrared_frame();
-
-        // Convert RealSense frame to OpenCV matrix:
-//        color_mat = frame_to_mat(color);
+        auto color_frame = data.get_color_frame();
 
 //        // Query frame size (width and height)
-//        const int w = color.as<rs2::video_frame>().get_width();
-//        const int h = color.as<rs2::video_frame>().get_height();
+//        const int w = depth.as<rs2::video_frame>().get_width();
+//        const int h = depth.as<rs2::video_frame>().get_height();
 //
 //        // Create OpenCV matrix of size (w,h) from the colorized depth data
-//        cv::Mat image(cv::Size(w, h), CV_8UC3, (void *) color.get_data(), cv::Mat::AUTO_STEP);
+//        Mat image(Size(w, h), CV_8UC3, (void *) depth.get_data(), Mat::AUTO_STEP);
 
-
-        colorMat = frame_to_mat(color);
-
+        // convert frame to mat
+        Mat image = frame_to_mat(color_frame);
         // Update the window with new data
-        imshow("rs_opencv", colorMat);
-
+        imshow(window_name, image);
     }
 
-    void onDraw(Graphics &g) {
-        // Update the window with new data
-//        imshow("display image", color_mat);
-
-
-    }
-};
-
-int main() {
-    WallApp app;
-    app.start();
+    return EXIT_SUCCESS;
 }
+catch (const rs2::error &e) {
+    std::cerr << "RealSense error calling " << e.get_failed_function() << "(" << e.get_failed_args() << "):\n    "
+              << e.what() << std::endl;
+    return EXIT_FAILURE;
+}
+catch (const std::exception &e) {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+}
+
 
 
