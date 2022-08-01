b5da6a3ab726f7afdb45e72242a79cb6d01e98b6
Leo Qian <a2573705387@gmail.com>
2022-08-01 11:35:27 -0400

    Revert "test window object in allolib"
    This reverts commit 8112ff8d7c28d57efdf4937fa8f7735320e9f008.
    
    # Conflicts:
    #	.vscode/settings.json
    #	tutorials/test/02_BoxWithDepth.cpp

--- tutorials/test/02_BoxWithDepth.cpp
+++ tutorials/test/02_BoxWithDepth.cpp
@@ -2,11 +2,9 @@
 #include "al/graphics/al_Shapes.hpp"
 #include "al/ui/al_Parameter.hpp"
 #include "al/math/al_Random.hpp"
-// #include "/Users/leoqian/Desktop/2022_summer_project/C++/librealsense/include/librealsense2/rs.hpp"
 #include <librealsense2/rs.hpp>
 // #include "example.hpp" 
 #include <iostream>
-#include "al/io/al_Window.hpp"
 
 using namespace al;
 // using namespace rs2;
@@ -14,15 +12,8 @@ using namespace al;
 class WallApp : public App {
 public:
 
-
 // mesh to store the points we're rendering
-VAOMesh verts;
-std::vector<Color> field;
-
-// store width and height of the window
-int width;
-int height;
-
+Mesh verts;
 
 // Declare pointcloud object, for calculating pointclouds and texture mappings
 rs2::pointcloud pc;
@@ -35,28 +26,24 @@ rs2::pipeline pipe;
 Texture tex;
 
 WallApp() {
-    // width = defaultWindow().width();
-    // height = defaultWindow().height();
+
 }
 
 void onCreate() {
+	
     
+
     nav().pullBack(16);
 
     
     // Configure and start the pipeline
     pipe.start();
-
-
 }
 
 void onAnimate(double dt) {
-    // width = defaultWindow().width();
-    // height = defaultWindow().height();
     
 
     auto frames = pipe.wait_for_frames();
-    // std::cout << frames.get_color_frame().get_width() << std::endl;
 
     auto color = frames.get_color_frame();
 
@@ -66,8 +53,6 @@ void onAnimate(double dt) {
 
     // Tell pointcloud object to map to this color frame
     pc.map_to(color);
-
-    // color.get();
 
     auto depth = frames.get_depth_frame();
 
@@ -82,21 +67,6 @@ void onAnimate(double dt) {
     /* this segment actually prints the pointcloud */
     auto vertices = points.get_vertices();              // get vertices
     auto tex_coords = points.get_texture_coordinates(); // and texture coordinates
-    
-    // configure textture
-    // tex.destroy();
-    // tex.create2D(width,height);
-    // tex.filterMag(Texture::LINEAR);
-
-    // int stride = tex.numComponents();
-    // int Nx = tex.width();
-    // int Ny = tex.height();
-
-    // // Get a pointer to the (client-side) pixel buffer.
-    // // When we make a read access to the pixels, they are flagged as dirty
-    // // and get sent to the GPU the next time the texture is bound.
-    // std::vector<unsigned char> pixels;
-    // pixels.resize(stride * Nx * Ny);
 
     verts.reset();
     for (int i = 0; i < points.size(); i++)
@@ -106,16 +76,13 @@ void onAnimate(double dt) {
             // upload the point and texture coordinates only for points we have depth data for
             verts.vertex(vertices[i].x,vertices[i].y,vertices[i].z);
             // glTexCoord2fv(tex_coords[i]);
-            verts.texCoord(tex_coords[i].u,tex_coords[i].v);
+            // verts.texCoord(tex_coords[i].u,tex_coords[i].v);
             
         }
     }
 }
 
 void onDraw(Graphics &g) {
-    // std::cout << defaultWindow().height() << " " << defaultWindow().width() << std::endl;
-    
-    
     g.clear(0,0,0);
 
     
