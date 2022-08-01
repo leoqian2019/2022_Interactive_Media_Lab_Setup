13991cabb6d37a9e5dc01ab56ea9639b518097ce
Leo Qian <a2573705387@gmail.com>
2022-08-01 11:37:37 -0400

    Revert "test tex object's functionality"
    This reverts commit ca3e106f

--- tutorials/test/02_BoxWithDepth.cpp
+++ tutorials/test/02_BoxWithDepth.cpp
@@ -2,7 +2,6 @@
 #include "al/graphics/al_Shapes.hpp"
 #include "al/ui/al_Parameter.hpp"
 #include "al/math/al_Random.hpp"
-// #include "/Users/leoqian/Desktop/2022_summer_project/C++/librealsense/include/librealsense2/rs.hpp"
 #include <librealsense2/rs.hpp>
 // #include "example.hpp" 
 #include <iostream>
@@ -19,10 +18,6 @@ public:
     VAOMesh verts;
     std::vector<Color> field;
 
-// store width and height of the window
-    int width;
-    int height;
-
 
 // Declare pointcloud object, for calculating pointclouds and texture mappings
     rs2::pointcloud pc;
@@ -35,28 +30,23 @@ public:
 Texture tex;
 
 WallApp() {
-    width = defaultWindow().width;
-    height = defaultWindow().height;
+
 }
 
 void onCreate() {
+    // window.makeCurrent();
+
 
     nav().pullBack(16);
 
-    
+
     // Configure and start the pipeline
     pipe.start();
-
-
 }
 
 void onAnimate(double dt) {
-    // width = defaultWindow().width();
-    // height = defaultWindow().height();
-
 
     auto frames = pipe.wait_for_frames();
-    std::cout << frames.get_color_frame().get_width() << std::endl;
 
     auto color = frames.get_color_frame();
 
@@ -66,8 +56,6 @@ void onAnimate(double dt) {
 
     // Tell pointcloud object to map to this color frame
     pc.map_to(color);
-
-    // color.get();
 
     auto depth = frames.get_depth_frame();
 
@@ -83,21 +71,6 @@ void onAnimate(double dt) {
     auto vertices = points.get_vertices();              // get vertices
     auto tex_coords = points.get_texture_coordinates(); // and texture coordinates
 
-    // configure textture
-    tex.destroy();
-    tex.create2D(width, height);
-    tex.filterMag(Texture::LINEAR);
-
-    int stride = tex.numComponents();
-    int Nx = tex.width();
-    int Ny = tex.height();
-
-    // Get a pointer to the (client-side) pixel buffer.
-    // When we make a read access to the pixels, they are flagged as dirty
-    // and get sent to the GPU the next time the texture is bound.
-    std::vector<unsigned char> pixels;
-    pixels.resize(stride * Nx * Ny);
-
     verts.reset();
     for (int i = 0; i < points.size(); i++) {
         if (vertices[i].z) {
@@ -105,13 +78,13 @@ void onAnimate(double dt) {
             verts.vertex(vertices[i].x, vertices[i].y, vertices[i].z);
             // glTexCoord2fv(tex_coords[i]);
             // verts.texCoord(tex_coords[i].u,tex_coords[i].v);
-            
+
         }
     }
 }
 
 void onDraw(Graphics &g) {
-    // std::cout << defaultWindow().height() << " " << defaultWindow().width() << std::endl;
+    std::cout << defaultWindow().height() << " " << defaultWindow().width() << std::endl;
 
 
     g.clear(0, 0, 0);
