7498995f803f35e8d9d9ef4fb3d1174bda4d3e25
Leo Qian <a2573705387@gmail.com>
2022-08-05 10:55:07 -0400

    update bash file to run oepncv with realsense

--- tutorials/test/rs_opencv/run_oepncv.sh
+++ tutorials/test/rs_opencv/run_oepncv.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-g++ -std=c++11 $1.cpp -o $1  -lrealsense2 -lopencv_core -lopencv_videoio -lopencv_highgui -lopencv_imgproc -lopencv_imgcodecs -lopencv_video
+g++ -std=c++11 $1 -o out  -lrealsense2 -lopencv_core -lopencv_videoio -lopencv_highgui -lopencv_imgproc -lopencv_imgcodecs -lopencv_video
+./out
