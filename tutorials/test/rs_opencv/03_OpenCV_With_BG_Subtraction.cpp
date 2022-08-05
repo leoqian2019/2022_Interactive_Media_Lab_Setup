7a8feefc930767cefa59ced6fcdf4bb84c4b6168
Leo Qian <a2573705387@gmail.com>
2022-08-04 21:43:10 -0400

    add 03_OpenCV_With_BG_Subtraction.cpp

--- tutorials/test/rs_opencv/03_OpenCV_With_BG_Subtraction.cpp
+++ tutorials/test/rs_opencv/03_OpenCV_With_BG_Subtraction.cpp
@@ -5,17 +5,20 @@
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/video.hpp>
+#include "librealsense2/rs.hpp" // Include RealSense Cross Platform API
+#include <opencv2/opencv.hpp>   // Include OpenCV API
+#include "cv-helpers.hpp"
 
 using namespace cv;
 using namespace std;
 const char *params
         = "{ help h         |           | Print usage }"
-          "{ input          | vtest.avi | Path to a video or a sequence of image }"
-          "{ algo           | MOG2      | Background subtraction method (KNN, MOG2) }";
+          "{ input          | vtest.avi | Path to a video or a sequence of image }";
 
 int main(int argc, char *argv[]) {
+
+    // handle parameter related to background subtraction
     CommandLineParser parser(argc, argv, params);
-//    parser.printMessage();
     parser.about("This program shows how to use background subtraction methods provided by "
                  " OpenCV. You can process both videos and images.\n");
     if (parser.has("help")) {
@@ -30,27 +33,36 @@ int main(int argc, char *argv[]) {
         pBackSub = createBackgroundSubtractorKNN();
     }
 
-    VideoCapture capture(samples::findFile(parser.get<String>("input")));
-    if (!capture.isOpened()) {
-        //error in opening the video input
-        cerr << "Unable to open: " << parser.get<String>("input") << endl;
-        return 0;
-    }
+    // handle realsense camera api
+    // Declare realsense config
+    rs2::config cfg;
+    cfg.enable_stream(RS2_STREAM_COLOR);
+
+    // Declare realsense align
+    rs2::align align_to(RS2_STREAM_COLOR);
+
+
+    // Declare RealSense pipeline, encapsulating the actual device and sensors
+    rs2::pipeline pipe;
+    // Start streaming with default recommended configuration
+    pipe.start();
+
+    using namespace cv;
+
+
     Mat frame, fgMask;
     while (true) {
-        capture >> frame;
-        if (frame.empty())
-            break;
+        rs2::frameset data = pipe.wait_for_frames(); // Wait for next set of frames from the camera
+
+        data = align_to.process(data);
+
+        auto color_frame = data.get_color_frame();
+        // convert frame to mat
+        frame = frame_to_mat(color_frame);
+
         //update the background model
         pBackSub->apply(frame, fgMask);
-        //get the frame number and write it on the current frame
-        rectangle(frame, cv::Point(10, 2), cv::Point(100, 20),
-                  cv::Scalar(255, 255, 255), -1);
-        stringstream ss;
-        ss << capture.get(CAP_PROP_POS_FRAMES);
-        string frameNumberString = ss.str();
-        putText(frame, frameNumberString.c_str(), cv::Point(15, 15),
-                FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0));
+
         //show the current frame and the fg masks
         imshow("Frame", frame);
         imshow("FG Mask", fgMask);
